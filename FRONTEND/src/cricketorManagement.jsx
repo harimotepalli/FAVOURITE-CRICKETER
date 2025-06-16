94901390bc823999bb2410407264571f6266990e
harimotepalli <durgahari012@gmail.com>
2025-06-16 19:19:35 +0530

    added a comment

--- FRONTEND/src/cricketorManagement.jsx
+++ FRONTEND/src/cricketorManagement.jsx
@@ -4,6 +4,8 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// cricketer management
+
 const CricketerManagement = () => {
     const apiBase = "http://localhost:7000/api/cricketers";  const [cricketers, setCricketers] = useState([]);
   const [formData, setFormData] = useState({
